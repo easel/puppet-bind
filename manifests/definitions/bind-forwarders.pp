a57fb8484b2327b4f847a61018f7e0abcfabe0ed
Erik LaBianca <erik.labianca@gmail.com>
2011-03-15 23:53:54 -0400

    add option to set up bind forwarders

---
+++ manifests/definitions/bind-forwarders.pp
@@ -0,0 +1,20 @@
+/*
+
+= Definition: bind::forwarders
+
+Set up bind forwards
+
+Arguments:
+ *$forwarders*:  List of IP addresses to forward DNS resolution to
+
+
+*/
+define bind::forwarders($forwarders=false) {
+    file {"/etc/bind/named.conf.options":
+      ensure => file,
+      owner => root,
+      group => root,
+      mode => 0755,
+      content => template("bind/named.conf.options.erb"),
+    }
+}
